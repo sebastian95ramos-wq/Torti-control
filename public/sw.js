75651c09b3934360ba88063781553fdd2781cc9e
sebastian95ramos-wq <sebastian95ramos@gmail.com>
2026-04-09 23:48:25 -0600

    Add files via upload
    nueva prueba 2

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,11 @@
-const CACHE_NAME = "torti-cache-v2";
-const urlsToCache = ["/","/index.html","/main.jsx"];
+﻿const CACHE_NAME = "torti-cache-v1";
 
-self.addEventListener("install", e=>{
- e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(urlsToCache)));
+self.addEventListener("install", e => {
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", e=>{
- e.respondWith(
-  caches.match(e.request).then(res=> res || fetch(e.request).catch(()=>caches.match("/")))
- );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => new Response("Sin conexión"))
+  );
 });
---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+﻿import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App />
+);
+
+// Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js");
+  });
+}
