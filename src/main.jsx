80723b5fbb37d543f4b6eadfc79cc4516df3bd70
sebastian95ramos-wq <sebastian95ramos@gmail.com>
2026-04-10 03:39:17 -0600

    Reorganize imports and clean up main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,13 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import './index.css'
 import { registerSW } from 'virtual:pwa-register'
 
 registerSW({ immediate: true })
-import App from "./App.jsx";
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-<React.StrictMode>
+  <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
-
-// Service Worker
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/sw.js");
-  });
-}
